630d9b634c5771f4186ab5d579fb309c0f4eee18
Tanmay <tanmaytambat01@gmail.com>
2024-06-17 18:39:30 +0530

    Modify by adding object rendering

--- 01vite-react-app/src/main.jsx
+++ 01vite-react-app/src/main.jsx
@@ -2,8 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+
+//Creating Custom React Element
+const reactElement=React.createElement(
+  "a",
+  {
+    href:"https://www.google.com",
+    target:"_blank"
+  },
+  "Click me to visit google"
+)
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App/>
+//   </React.StrictMode>,
+// )
+//<App /> can also be written as App()
+//OR
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    reactElement
 )
