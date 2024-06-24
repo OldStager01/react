a7869c4ed40abf68f36f0ab6be93d5fe35d6d392
Tanmay <tanmaytambat01@gmail.com>
2024-06-25 02:47:45 +0530

    todo using redux-toolkit

---
+++ 11todo-reduxToolkit/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
+
+function App() {
+
+  return (
+    <>
+      Redus-Toolkit with React
+      <AddTodo/>
+      <Todos/>
+    </>
+  )
+}
+
+export default App
